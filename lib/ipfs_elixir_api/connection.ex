ea00ad07f9104c51fac541871af2cc89fa2d9aad
tensor-programming <abeltensor@tensor-programming.com>
2017-10-09 20:52:49 -0400

    addeed update

--- lib/ipfs_elixir_api/connection.ex
+++ lib/ipfs_elixir_api/connection.ex
@@ -1,13 +1,15 @@
 defmodule IpfsConnection do
+    #add struct for later use. 
     defstruct host: "localhost", port: 5001, base: "api/v0", protocol: "http" 
     use Tesla
     
+    #boundary for add_cmd function Todo: implement add_cmd funciton. 
     @boundary "7MA4YWxkTrZu0gW"
 
     plug Tesla.Middleware.BaseUrl, "http://localhost:5001/api/v0"
     plug Tesla.Middleware.JSON
 
-
+    
     def get_cmd(multihash) do
         res = requests("/get?arg=", multihash)
         res |> write_file(multihash)
@@ -22,10 +24,19 @@ defmodule IpfsConnection do
         requests("/swarm/peers", "")
     end
 
+    #Ls cmd Todo  Implement proper Json Format. 
     def ls_cmd(multihash) do
         res = requests("/ls?arg=", multihash)
-        res.body
+        res
     end
+
+    #Update function - Todo (correctly format res.body string)
+    def update(args) do
+        res = requests("/update?arg=", args)
+        String.replace(res, "\n", " ")
+    end
+
+
 
     defp build_url(conn, path) do
         "#{conn.protocol}://#{conn.host}:#{conn.port}/#{conn.base}/#{path}"
@@ -58,7 +69,7 @@ defmodule IpfsConnection do
     end
 
     defp write_file(raw, multihash) do
-      File.write(multihash, raw, [:write])
+      File.write(multihash, raw, [:write, :utf8])
     end
     
 end
