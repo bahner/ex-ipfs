89b97272622606069921b6dcacc8855b5b5ebeec
tensor-programming <abeltensor@tensor-programming.com>
2017-10-11 00:12:12 -0400

    fixed tests for refactor

--- test/ipfs_elixir_api_test.exs
+++ test/ipfs_elixir_api_test.exs
@@ -3,9 +3,10 @@ defmodule IpfsElixirApiTest do
   doctest IpfsConnection
 
   import IpfsConnection
+  alias IpfsElixir.Api
 
   test "get_cmd should return a map when passed a valid key" do
-    bin = get_cmd("QmP3LE29sJf8vsnTG8dMeDEcrfoW2cyUG5XVUNKWZxg38i")
+    bin = Api.get_cmd("QmP3LE29sJf8vsnTG8dMeDEcrfoW2cyUG5XVUNKWZxg38i")
     assert is_map(bin)
     assert is_binary(bin.body)
     assert is_map(bin.headers)
@@ -13,7 +14,7 @@ defmodule IpfsElixirApiTest do
   end
 
   test "get_cmd should return an error when passed nothing or invalid key" do 
-    bin = get_cmd("test_case")
+    bin = Api.get_cmd("test_case")
     assert is_map(bin)
     assert bin.body["Message"] === "invalid 'ipfs ref' path"
     assert bin.body["Code"] === 0
