9e346a8be18351b34604e460f110052d1641971a
tensor-programming <abeltensor@tensor-programming.com>
2017-10-12 19:21:31 -0400

    added license and added library to hex.pm

--- mix.exs
+++ mix.exs
@@ -4,11 +4,15 @@ defmodule IpfsElixir.Mixfile do
   def project do
     [
       app: :ipfs_elixir,
-      version: "0.1.0",
+      version: "0.0.4",
       elixir: "~> 1.5",
       name: "IPFS Elixir",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package,
+      description: description(),
+      name: "Ipfs_Elixir_Api",
+      source_url: "https://github.com/tensor-programming/Elixir-Ipfs-Api"
     ]
   end
 
@@ -25,6 +29,19 @@ defmodule IpfsElixir.Mixfile do
       {:tesla, "~> 0.9.0"},
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "An Ipfs API wrapper build in Elixir"
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Jeremiah King"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/tensor-programming/Elixir-Ipfs-Api"}
     ]
   end
 end
