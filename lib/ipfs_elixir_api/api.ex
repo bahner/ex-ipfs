9e63a9089761764cd689ed7540c8d52bbf946790
tensor-programming <abeltensor@tensor-programming.com>
2017-10-13 03:30:23 -0400

    added a the object cmds.

--- lib/ipfs_elixir_api/api.ex
+++ lib/ipfs_elixir_api/api.ex
@@ -11,8 +11,7 @@ defmodule IpfsElixir.Api do
         <<0, 19, 148, 0, ... >>
   """
 
-    ## TODO: add (files, name, object, pin, p2p,
-    ##  filestore, shutdown, repo, resolve, stats, tar)
+    ## TODO: add (files, name, p2p, filestore, shutdown, repo, resolve, stats, tar)
 
     import IpfsConnection
     
@@ -30,46 +29,25 @@ defmodule IpfsElixir.Api do
     end
 
     # TODO: add various flags to the add_cmd. 
-    def add_cmd(file_path) do
-        #file_data = file_path |> File.open!([:read, :binary]) |> IO.binread(:all)
-        setup_multipart_form(file_path) |> request_post("/add")
-    end
+    def add_cmd(file_path), do: setup_multipart_form(file_path) |> request_post("/add")
 
-    def id do
-        request_get("/id")
-        ##TODO: add extra args
-    end
+    def id, do: request_get("/id")
+        
+    def dns(arg) when is_bitstring(arg), do: request_get("/dns?arg=", arg)
 
-    def dns(arg) when is_bitstring(arg) do
-        request_get("/dns?arg=", arg)
-    end
- 
     ## TODO: add get_cmd for output, archive, compress and compression level
-    def get_cmd(multihash) when is_bitstring(multihash) do
-        request_get("/get?arg=", multihash)
-        ##TODO add optional file writing funcitonality.
-    end
+    def get_cmd(multihash) when is_bitstring(multihash), do: request_get("/get?arg=", multihash)
 
-    def cat_cmd(multihash) when is_bitstring(multihash) do
-        request_get("/cat?arg=", multihash)
-    end
+    def cat_cmd(multihash) when is_bitstring(multihash), do: request_get("/cat?arg=", multihash)
 
     #Ls cmd TODO  Implement proper Json Format.
-    def ls_cmd(multihash) when is_bitstring(multihash) do
-        request_get("/ls?arg=", multihash)
-    end
+    def ls_cmd(multihash) when is_bitstring(multihash), do: request_get("/ls?arg=", multihash)
 
-    def bitswap_ledger(peer_id) do
-        request_get("/bitswap/ledger?arg=", peer_id)
-    end
+    def bitswap_ledger(peer_id), do: request_get("/bitswap/ledger?arg=", peer_id)
+        
+    def bitswap_stat, do: request_get("/bitswap/stat")
 
-    def bitswap_stat do
-        request_get("/bitswap/stat")
-    end 
-
-    def bitswap_unwant(keys) do
-        request_get("/bitswap/unwant?arg=", keys)
-    end
+    def bitswap_unwant(keys), do: request_get("/bitswap/unwant?arg=", keys)
 
     def bitswap_wantlist(peer \\ "") do
         if peer != "" do
@@ -79,110 +57,58 @@ defmodule IpfsElixir.Api do
         end
     end
 
-    def block_get(arg) do
-        request_get("/block/get?arg=", arg)
-    end
+    def block_get(arg), do: request_get("/block/get?arg=", arg)  
 
-    def block_put(file_path) do
-        setup_multipart_form(file_path) |> request_post("/block/put")
-    end
+    def block_put(file_path), do: setup_multipart_form(file_path) |> request_post("/block/put")
 
-    def block_rm(multihash) do
-        request_get("/block/rm?arg=", multihash)
-    end
+    def block_rm(multihash), do: request_get("/block/rm?arg=", multihash)
 
-    def block_stat(multihash) do
-        request_get("/block/stat?arg=", multihash)
-    end
+    def block_stat(multihash), do: request_get("/block/stat?arg=", multihash)
 
-    def bootstrap_add_default do
-        request_get("/bootstrap/add/default")
-    end
+    def bootstrap_add_default, do: request_get("/bootstrap/add/default")
+        
+    def bootstrap_list, do: request_get("/bootstrap/list")
+        
+    def bootstrap_rm_all, do: request_get("bootstrap/rm/all")
 
-    def bootstrap_list do
-        request_get("/bootstrap/list")
-    end
+    def config_edit, do: request_get("/config/edit")
 
-    def bootstrap_rm_all do
-        request_get("bootstrap/rm/all")
-    end
+    def config_replace(file_path), do: setup_multipart_form(file_path) |> request_post("/config/replace")        
 
-    def config_edit do
-        request_get("/config/edit")
-    end
+    def config_show, do: request_get("/config/show")
 
-    def config_replace(file_path) do
-        setup_multipart_form(file_path) |> request_post("/config/replace")        
-    end
+    def dag_get(object), do: request_get("/dag/get?arg=", object)
 
-    def config_show do
-        request_get("/config/show")
-    end
+    def dag_put(file_path), do: setup_multipart_form(file_path) |> request_post("/dag/put")
 
-    def dag_get(object) do
-        request_get("/dag/get?arg=", object)
-    end
+    def swarm_peers, do: request_get("/swarm/peers")
 
-    def dag_put(file_path) do
-        setup_multipart_form(file_path) |> request_post("/dag/put")
-    end
+    def swarm_disconnect(multihash) when is_bitstring(multihash), do: request("/swarm/disconnect?arg=", multihash)
 
-    def swarm_peers do
-        request_get("/swarm/peers")
-    end
-
-    def swarm_disconnect(multihash) when is_bitstring(multihash) do
-        request("/swarm/disconnect?arg=", multihash)
-    end
-
-    def commands do
-        request_get("/commands")
-    end
+    def commands, do: request_get("/commands")
 
     ##Currently throws an error due to the size of JSON response.
-    def repo_verify do
-        request_get("/repo/verify")
-    end
+    def repo_verify, do: request_get("/repo/verify")
 
-    def ping(id) do
-        request("/ping?arg=", id)
-    end
+    def ping(id), do: request("/ping?arg=", id)
 
-    def log_level(subsys, level) do
-        request_get("/log/level?arg=" <> subsys <> "&arg=" <> level) 
-    end
+    def log_level(subsys, level), do: request_get("/log/level?arg=" <> subsys <> "&arg=" <> level) 
+        
+    def log_ls, do: request_get("/log/ls")
 
-    def log_ls do
-        request_get("/log/ls")
-    end
+    def log_tail, do: request_get("/log/tail")
 
-    def log_tail do
-        request_get("/log/tail")
-    end
+    def mount, do: request_get("/mount")
 
-    def mount do
-        request_get("/mount")
-    end
+    def pubsub_ls, do: request_get("/pubsub/ls")
 
-    def pubsub_ls do
-        request_get("/pubsub/ls")
-    end
+    def pubsub_peers, do: request_get("/pubsub/pub")
 
-    def pubsub_peers do
-        request_get("/pubsub/pub")
-    end
+    def pubsub_pub(topic, data), do: request_get("/pubsub/pub?arg=" <> topic <> "&arg=" <> data)
 
-    def pubsub_pub(topic, data) do
-        request_get("/pubsub/pub?arg=" <> topic <> "&arg=" <> data)
-    end
+    def pubsub_sub(topic), do: request_get("/pubsub/sub?arg=", topic)
 
-    def pubsub_sub(topic) do
-        request_get("/pubsub/sub?arg=", topic)
-    end
-
-    def refs_local do
-        request_get("/refs/local")
-    end
+    def refs_local, do: request_get("/refs/local")
 
     #Update function  - takes in the current args for update.
     def update(args) when is_bitstring(args) do
@@ -195,49 +121,27 @@ defmodule IpfsElixir.Api do
        request_get("/version?number=" <> to_string(num) <> "&commit=" <> to_string(comm) <> "&repo=" <> to_string(repo) <> "&all=" <> to_string(all) , "")
     end
 
-    def tour_list do
-        request_get("/tour/list")
-    end
+    def tour_list, do: request_get("/tour/list")
 
-    def tour_next do
-        request_get("/tour/next")
-    end
+    def tour_next, do: request_get("/tour/next")
 
-    def tour_restart do
-        request_get("/tour/restart")
-    end
+    def tour_restart, do: request_get("/tour/restart")
+        
+    def dht_find_peer(arg), do: request_get("/dht/findpeer?arg=", arg)
 
-    def dht_find_peer(arg) do
-        request_get("/dht/findpeer?arg=", arg)
-    end
+    def dht_find_provs(arg), do: request_get("/dht/findprovs?arg=", arg)
+        
+    def dht_get(arg), do: request_get("/dht/get?arg=", arg)
+        
+    def dht_provide(arg), do: request_get("/dht/provide?arg=", arg)
+        
+    def dht_put(key, value), do: request_get("/dht/put?arg=" <> key <> "&arg=" <> value)  
 
-    def dht_find_provs(arg) do
-        request_get("/dht/findprovs?arg=", arg)
-    end
+    def dht_query(peer_id), do: request_get("/dht/query?arg=", peer_id)
+        
+    def cmds_clear, do: request_get("/cmds/clear")
 
-    def dht_get(arg) do
-        request_get("/dht/get?arg=", arg)
-    end
-
-    def dht_provide(arg) do
-        request_get("/dht/provide?arg=", arg)
-    end
-
-    def dht_put(key, value) do  
-        request_get("/dht/put?arg=" <> key <> "&arg=" <> value)
-    end
-
-    def dht_query(peer_id) do
-        request_get("/dht/query?arg=", peer_id)
-    end
-
-    def cmds_clear do
-        request_get("/cmds/clear")
-    end
-
-    def cmds_set_time(time) do
-        request_get("/cmds/set-time?arg=", time)
-    end
+    def cmds_set_time(time), do: request_get("/cmds/set-time?arg=", time)
 
     def diag_net(vis \\ "") do
         if vis != "" do
@@ -247,34 +151,20 @@ defmodule IpfsElixir.Api do
         end
     end
 
-    def diag_sys do
-        request_get("/diag/sys")
-    end
+    def diag_sys, do: request_get("/diag/sys")
+        
+    def key_gen(key), do: request_get("/key/gen?arg=", key)
+        
+    def key_list, do: request_get("/key/list")
 
-    def key_gen(key) do
-        request_get("/key/gen?arg=", key)
-    end
+    def object_data(multihash), do: request_get("/object/data?arg=", multihash)
 
-    def key_list do
-        request_get("/key/list")
-    end
-
-    def object_data(multihash) do
-        request_get("/object/data?arg=", multihash)
-    end
-
-    def object_diff(obj_a, obj_b) do
-        request_get("/object/diff?arg=" <> obj_a <> "&arg=" <> obj_b)
-    end
-
-    def object_get(multihash) do
-        request_get("/object/get?arg=", multihash)
-    end
-
-    def object_links(multihash) do
-        request_get("/object/links?arg=", multihash)
-    end
-
+    def object_diff(obj_a, obj_b), do: request_get("/object/diff?arg=" <> obj_a <> "&arg=" <> obj_b)
+  
+    def object_get(multihash), do: request_get("/object/get?arg=", multihash)
+        
+    def object_links(multihash), do: request_get("/object/links?arg=", multihash)
+        
     def object_new(template \\ "") do
         if template != "" do
             request_get("/object/new?arg=", template)
@@ -282,6 +172,34 @@ defmodule IpfsElixir.Api do
             request_get("/object/new")
         end
     end
+
+    def object_patch_add_link(multihash, name, object), do: request_get("/object/patch/add-link?arg=" <> multihash <> 
+                                                                   "&arg=" <> name <> "&arg=" <> object)
+    def object_patch_append_data(multihash, data) do
+        setup_multipart_form(data) |> request_post("/object/patch/append-data?arg=" <> multihash)
+    end
+
+    def object_patch_rm_link(multihash, name), do: request_get("/object/patch/rm-link?arg=" <> multihash <> "&arg=", name)
+
+    def object_patch_set_data(multihash, data) do
+        setup_multipart_form(data) |> request_post("/object/patch/set-data?arg=", multihash)
+    end
+
+    def object_put(data), do: setup_multipart_form(data) |> request_post("/object/put")
+
+    def object_stat(multihash), do: request_get("/object/stat?arg=", multihash)
+
+    def pin_add(object), do: request_get("/pin/add?arg=", object)
+
+    def pin_ls(object \\ "") do
+        if object != "" do
+            request_get("/pin/ls?arg=", object)
+        else
+            request_get("/pin/ls")
+        end 
+    end
+    
+    def pin_rm(object), do: request_get("/pin/rm?arg=", object)
     
     defp shutdown(pid, term \\ []) do
         Process.exit(pid, term)
