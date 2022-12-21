d42240c414995044902db2f4651faa7f976d0c3b
Lars Bahner <lars.bahner@gmail.com>
2022-12-21 22:11:23 +0100

    ipfs.ex: add specs and docs

--- lib/ipfs.ex
+++ lib/ipfs.ex
@@ -62,6 +62,39 @@ defmodule MyspaceIPFS do
   @debug && plug(Tesla.Middleware.Logger)
 
   @doc """
+  Filter out any empty values from a list.
+  Removes nil, {}, [], and "".
+  """
+  @spec filter_empties(list) :: list
+  def filter_empties(list) do
+    list
+    |> Enum.filter(fn x -> x != nil end)
+    |> Enum.filter(fn x -> x != {} end)
+    |> Enum.filter(fn x -> x != [] end)
+    |> Enum.filter(fn x -> x != "" end)
+  end
+
+  @doc """
+  Extracts the data from a response. Given a response, it will structure the
+  data in a way that is easier to work with. IPFS only sends strings. This
+  function will convert the string to a list of maps.
+  """
+  @spec map_response_data(any) :: list
+  def map_response_data(response) do
+    extract_data_from_plain_response(response)
+    |> filter_empties()
+    |> convert_list_of_tuples_to_map()
+  end
+
+  @doc """
+  Wraps the data in an elixir standard response tuple.
+  {:ok, data} or {:error, data}
+  """
+  @spec okify(any) :: {:ok, any} | {:error, any}
+  def okify({:error, _} = err), do: err
+  def okify(res), do: {:ok, res}
+
+  @doc """
   High level function allowing to perform POST requests to the node.
   A `path` has to be provided, along with an optional list of `opts` that are
   dependent on the endpoint that will get hit.
@@ -91,19 +124,76 @@ defmodule MyspaceIPFS do
     end
   end
 
-  # Handles the response from the node. It returns the body of the response
-  # if the status code is 200, otherwise it returns an error tuple.
+  @doc """
+  Unlists a list if it only contains one element.
+  """
+  @spec unlist(list) :: any
+  def unlist(list) do
+    case list do
+      [x] -> x
+      _ -> list
+    end
+  end
 
-  # ## Status codes that are handled
-  # https://docs.ipfs.tech/reference/kubo/rpc/#http-status-codes
+  # Private functions
 
-  #   - 200 - The request was processed or is being processed (streaming)
-  #   - 500 - RPC Endpoint returned an error
-  #   - 400 - Malformed RPC, argument type error, etc.
-  #   - 403 - RPC call forbidden
-  #   - 404 - RPC endpoint does not exist
-  #   - 405 - RPC endpoint exists but method is not allowed
+  defp convert_list_of_tuples_to_map(list) do
+    list
+    |> filter_empties()
+    |> Enum.map(fn x -> list_of_tuples_to_map(x) end)
+  end
+
+  defp extract_data_from_plain_response(binary) do
+    binary
+    |> split_string_by_newline()
+    |> filter_empties()
+    |> Enum.map(fn x -> extract_tuples_from_string(x) end)
+  end
+
+  defp extract_tuples_from_string(string) do
+    string
+    |> split_string_by_comma()
+    |> filter_empties()
+    |> Enum.map(fn x -> tuplestring_to_tuple(x) end)
+  end
+
+  defp filter_empty_keys(map) do
+    map
+    |> Enum.reject(fn {x, _} -> is_nil(x) end)
+    |> Enum.reject(fn {x, _} -> x == "" end)
+  end
+
+  defp get_value_from_string(string) do
+    # IO.puts("ValueString: #{string}")
+    with data = Regex.run(~r/:"(.+?)"/, string),
+         true <- not is_nil(data) do
+      Enum.at(data, 1)
+    else
+      _ -> nil
+    end
+  end
+
+  defp get_name_from_string(string) do
+    # IO.puts("NameString: #{string}")
+    with data = Regex.run(~r/"(.+?)":/, string),
+         true <- not is_nil(data) do
+      Enum.at(data, 1)
+    else
+      _ -> nil
+    end
+  end
+
   defp handle_response(response) do
+    # Handles the response from the node. It returns the body of the response
+    # if the status code is 200, otherwise it returns an error tuple.
+    # ## Status codes that are handled
+    # https://docs.ipfs.tech/reference/kubo/rpc/#http-status-codes
+    #   - 200 - The request was processed or is being processed (streaming)
+    #   - 500 - RPC Endpoint returned an error
+    #   - 400 - Malformed RPC, argument type error, etc.
+    #   - 403 - RPC call forbidden
+    #   - 404 - RPC endpoint does not exist
+    #   - 405 - RPC endpoint exists but method is not allowed
     case response do
       {:ok, %Tesla.Env{status: 200, body: body}} -> body
       {:ok, %Tesla.Env{status: 500}} -> {:error, response}
@@ -115,6 +205,13 @@ defmodule MyspaceIPFS do
     end
   end
 
+  defp list_of_tuples_to_map(list) do
+    list
+    |> filter_empties()
+    |> Enum.into(%{})
+    |> filter_empty_keys()
+  end
+
   defp multipart(fspath) do
     Multipart.new()
     |> Multipart.add_file(fspath,
@@ -122,38 +219,6 @@ defmodule MyspaceIPFS do
       filename: "#{fspath}",
       detect_content_type: true
     )
-  end
-
-  @spec okify(any) :: {:ok, any} | {:error, any}
-  def okify({:error, _} = err), do: err
-  def okify(res), do: {:ok, res}
-
-
-  @spec map_response_data(any) :: list
-  def map_response_data(response) do
-    extract_data_from_plain_response(response)
-    |> convert_list_of_tuples_to_map()
-  end
-
-  # Private functions
-  defp extract_data_from_plain_response(binary) do
-    binary
-    |> split_string_by_newline()
-    |> filter_empties()
-    |> Enum.map(fn x -> extract_tuples_from_string(x) end)
-  end
-
-  defp convert_list_of_tuples_to_map(list) do
-    list
-    |> Enum.map(fn x -> list_of_tuples_to_map(x) end)
-  end
-
-  defp filter_empties(list) do
-    list
-    |> Enum.filter(fn x -> x != nil end)
-    |> Enum.filter(fn x -> x != {} end)
-    |> Enum.filter(fn x -> x != [] end)
-    |> Enum.filter(fn x -> x != "" end)
   end
 
   defp split_string_by_newline(string) do
@@ -164,41 +229,7 @@ defmodule MyspaceIPFS do
     Regex.split(~r/,/, string)
   end
 
-  defp extract_tuples_from_string(string) do
-    string
-    |> split_string_by_comma()
-    |> filter_empties()
-    |> Enum.map(fn x -> tuplestring_to_tuple(x) end)
-  end
-
-  defp get_value_from_string(string) do
-    # IO.puts("ValueString: #{string}")
-    with data = Regex.run(~r/:"(.+?)"/, string),
-         true <- not is_nil(data) do
-      Enum.at(data, 1)
-    else
-      _ -> ""
-    end
-  end
-
-  defp get_name_from_string(string) do
-    # IO.puts("NameString: #{string}")
-    with data = Regex.run(~r/"(.+?)":/, string),
-         true <- not is_nil(data) do
-      Enum.at(data, 1)
-    else
-      _ -> ""
-    end
-  end
-
   defp tuplestring_to_tuple(string) do
     {get_name_from_string(string), get_value_from_string(string)}
   end
-
-  def list_of_tuples_to_map(list) do
-    list
-    |> Enum.into(%{})
-  end
-
-
 end
