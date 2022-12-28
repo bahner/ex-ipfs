4a4030dd876025efca07d98a180b37b4c5821fa1
Lars Bahner <lars.bahner@gmail.com>
2022-12-28 20:57:09 +0100

    Update version group

--- lib/version.ex
+++ lib/version.ex
@@ -6,18 +6,51 @@ defmodule MyspaceIPFS.Version do
   import MyspaceIPFS.Utils
 
   @typep result :: MyspaceIPFS.result()
-  @typep opts :: MyspaceIPFS.opts()
 
-  # version function - does not currently accept the optional arguments on golang client.
-  @spec version(opts) :: result
-  def version(opts \\ nil) do
-    post_query("/version", opts)
+  @doc """
+  Get the version of the IPFS daemon.
+
+  ## Parameters
+  What version information to return (optional). Defaults to all.
+  Allowed values are: all, Commit, Golang, Version, Repo, System.
+
+  ## Options
+  https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-version
+  """
+  @spec version(binary) :: result
+  def version(name \\ "all") do
+    post_query("/version")
+    |> map_response_data()
+    |> List.flatten()
+    |> return_version(name)
     |> okify()
   end
 
+  defp return_version(versions, key) do
+    if key == "all" do
+      versions
+    else
+      get_version(versions, key)
+    end
+  end
+
+  defp get_version(versions, key) do
+    with versions <- Enum.find(versions, fn {k, _v} -> k == key end) do
+      try do
+        versions |> elem(1)
+      rescue
+        _ in ArgumentError -> {:error, "Invalid key: #{key}"}
+      end
+    end
+  end
+
+  @doc """
+  Get the depdency versions of the IPFS daemon.
+  """
   @spec deps() :: result
   def deps() do
     post_query("/version/deps")
+    |> map_response_data()
     |> okify()
   end
 end
