6c9a05d2ca4df0baca6ad3af9641f3ecf3255fb6
Lars Bahner <lars.bahner@gmail.com>
2022-12-29 20:26:42 +0100

    Relax version and depedencies to be more forgiving.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MyspaceIPFS.Mixfile do
     [
       app: :myspace_ipfs,
       version: "0.1.0-alpha.1",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       name: "Myspace IPFS",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -24,9 +24,9 @@ defmodule MyspaceIPFS.Mixfile do
 
   defp deps do
     [
-      {:tesla, "~> 1.4.4"},
-      {:jason, "~> 1.4.0"},
-      {:nanoid, "~> 2.0.5"},
+      {:tesla, "~> 1.4"},
+      {:jason, "~> 1.4"},
+      {:nanoid, "~> 2.0"},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
