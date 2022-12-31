6ec721c867aa68f056b5a50c189f5e24b331df57
Lars Bahner <lars.bahner@gmail.com>
2022-12-31 20:20:13 +0100

    Add filestore commands.

--- lib/filestore.ex
+++ lib/filestore.ex
@@ -3,19 +3,53 @@ defmodule MyspaceIPFS.Filestore do
   MyspaceIPFS.Api is where the filestore commands of the IPFS API reside.
   """
   import MyspaceIPFS.Api
+  import MyspaceIPFS.Utils
+
+  @typep okresult :: MyspaceIPFS.okresult()
+  @typep opts :: MyspaceIPFS.opts()
 
   @doc """
   List blocks that are both in the filestore and standard block storage.
   """
-  def dups, do: post_query("/filestore/dups")
+  @spec dups() :: okresult
+  def dups do
+     post_query("/filestore/dups")
+     |> handle_json_response()
+  end
 
   @doc """
   List objects in the filestore.
-  """
-  def ls, do: post_query("/filestore/ls")
 
-  @doc """
-  verify that objects in the filestore are not corrupted.
+  ## Options
+  https://docs.ipfs.io/reference/http/api/#api-v0-filestore-ls
+  ```
+  [
+    arg: <string>, # CID of objects to list.
+    file-order: <bool>, # sort the results based on the path of the backing file.
+  ]
+  ```
   """
-  def verify, do: post_query("/filestore/verify")
+  @spec ls(opts) :: okresult
+  def ls(opts \\ []) do
+     post_query("/filestore/ls", query: opts)
+      |> handle_json_response()
+  end
+
+   @doc """
+  Verify objects in the filestore.
+
+  ## Options
+  https://docs.ipfs.io/reference/http/api/#api-v0-filestore-verify
+  ```
+  [
+    arg: <string>, # CID of objects to list.
+    file-order: <bool>, # sort the results based on the path of the backing file.
+  ]
+  ```
+  """
+  @spec verify(opts) :: okresult
+  def verify(opts \\ []) do
+     post_query("/filestore/verify", query: opts)
+     |> handle_json_response()
+  end
 end
