b4594c79cfda31b7fed5c88b618be9d49f0d59f1
Lars Bahner <lars.bahner@gmail.com>
2022-12-31 17:23:22 +0100

    Add last standing dht command.

--- lib/dht.ex
+++ lib/dht.ex
@@ -3,13 +3,26 @@ defmodule MyspaceIPFS.Dht do
   MyspaceIPFS.Api.Dht is where the dht commands of the IPFS API reside.
   """
   import MyspaceIPFS.Api
+  import MyspaceIPFS.Utils
 
-  # DHT COMMANDS
-  # NB: findpeer is deprecated.
-  # NB: findprovs is deprecated.
-  # NB: get is deprecated.
-  # NB: provide is deprecated.
-  # NB: put is deprecated.
+  @typep peer_id :: MyspaceIPFS.peer_id()
 
-  def query(peer_id), do: post_query("/dht/query?arg=" <> peer_id)
+  @doc """
+  Find the closest peers to a given key.
+
+  ## Parameters
+  `peer_id` - The peer ID to find the closest peers to.
+
+  ## Options
+  https://docs.ipfs.tech/reference/kubo/rpc/#api-v0-dht-query
+  ```
+  [
+    `verbose` - <bool>, # Print extra information.
+  ]
+  ```
+  """
+  def query(peer_id) do
+    post_query("/dht/query?arg=" <> peer_id)
+    |> handle_data_response()
+  end
 end
