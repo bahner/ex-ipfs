44f6ef37a675c0cf49e50344824924d7af33bde6
Lars Bahner <lars.bahner@gmail.com>
2023-01-22 01:21:58 +0100

    Lower noise level for now.

--- config/config.exs
+++ config/config.exs
@@ -6,3 +6,8 @@ config :tesla,
 config :myspace_ipfs,
   api_url: "http://localhost:5001/api/v0",
   default_topic: "ubXlzcGFjZQ"
+
+config :logger,
+  compile_time_purge_matching: [
+    [level_lower_than: :error]
+  ]
