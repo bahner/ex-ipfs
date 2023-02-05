6b609cf849be8b391f0d570b6fa6277627fbea9f
Lars Bahner <lars.bahner@gmail.com>
2023-02-05 02:02:48 +0100

    Fix CidCid -> CidBase32 rename for test

--- test/myspace_ipfs/cid_test.exs
+++ test/myspace_ipfs/cid_test.exs
@@ -13,7 +13,7 @@ defmodule MyspaceIPFS.CidTest do
   test "check base32 conversion" do
     {:ok, cid} = Cid.base32("QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N")
     assert is_map(cid)
-    assert %MyspaceIPFS.CidCid{} = cid
+    assert %MyspaceIPFS.CidBase32CID{} = cid
     assert cid.cid_str == "QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"
     assert cid.formatted == "bafybeie5745rpv2m6tjyuugywy4d5ewrqgqqhfnf445he3omzpjbx5xqxe"
     assert cid.error_msg == ""
@@ -22,7 +22,7 @@ defmodule MyspaceIPFS.CidTest do
   test "format returns the same for v0" do
     {:ok, cid} = Cid.format("QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N")
     assert is_map(cid)
-    assert %MyspaceIPFS.CidCid{} = cid
+    assert %MyspaceIPFS.CidBase32CID{} = cid
     assert cid.cid_str == "QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"
     assert cid.formatted == "QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"
     assert cid.error_msg == ""
@@ -31,7 +31,7 @@ defmodule MyspaceIPFS.CidTest do
   test "format returns the same for v1" do
     {:ok, cid} = Cid.format("bafybeie5745rpv2m6tjyuugywy4d5ewrqgqqhfnf445he3omzpjbx5xqxe")
     assert is_map(cid)
-    assert %MyspaceIPFS.CidCid{} = cid
+    assert %MyspaceIPFS.CidBase32CID{} = cid
     assert cid.cid_str == "bafybeie5745rpv2m6tjyuugywy4d5ewrqgqqhfnf445he3omzpjbx5xqxe"
     assert cid.formatted == "bafybeie5745rpv2m6tjyuugywy4d5ewrqgqqhfnf445he3omzpjbx5xqxe"
     assert cid.error_msg == ""
@@ -40,7 +40,7 @@ defmodule MyspaceIPFS.CidTest do
   test "format v1" do
     {:ok, cid} = Cid.format("QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N", v: 1)
     assert is_map(cid)
-    assert %MyspaceIPFS.CidCid{} = cid
+    assert %MyspaceIPFS.CidBase32CID{} = cid
     assert cid.cid_str == "QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"
     assert cid.formatted == "zdj7Wg4epE5TV2MzXASh5WY5EBarz9hAhno1FwJS2VtLBGrFJ"
     assert cid.error_msg == ""
@@ -49,7 +49,7 @@ defmodule MyspaceIPFS.CidTest do
   test "format v0" do
     {:ok, cid} = Cid.format("bafybeie5745rpv2m6tjyuugywy4d5ewrqgqqhfnf445he3omzpjbx5xqxe", v: 0)
     assert is_map(cid)
-    assert %MyspaceIPFS.CidCid{} = cid
+    assert %MyspaceIPFS.CidBase32CID{} = cid
     assert cid.cid_str == "bafybeie5745rpv2m6tjyuugywy4d5ewrqgqqhfnf445he3omzpjbx5xqxe"
     assert cid.formatted == "QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"
     assert cid.error_msg == ""
