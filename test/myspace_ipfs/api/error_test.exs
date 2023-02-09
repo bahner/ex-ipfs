5a1da6b14a327d6e608bdac4fc282cca5893b4db
Lars Bahner <lars.bahner@gmail.com>
2023-02-09 22:54:44 +0100

    Add ApiError unit test

---
+++ test/myspace_ipfs/api/error_test.exs
@@ -0,0 +1,18 @@
+defmodule MyspaceIPFS.ApiErrorTest do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+
+  alias MyspaceIPFS.ApiError, as: ApiError
+
+  test "fails on missing data" do
+    catch_error(%ApiError{} = ApiError.new())
+  end
+
+  test "test creation of error" do
+    assert %ApiError{} = ApiError.new(%{"Message" => "message", "Code" => 0})
+    e = ApiError.new(%{"Message" => "message", "Code" => 0})
+    assert e.message == "message"
+    assert e.code == 0
+  end
+end
