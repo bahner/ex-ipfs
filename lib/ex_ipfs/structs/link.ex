e339882018567ba81eca8486e9d6c1fe62e23f51
Lars Bahner <lars.bahner@gmail.com>
2023-02-21 23:40:26 +0100

    Struct Link

--- lib/ex_ipfs/structs/link.ex
+++ lib/ex_ipfs/structs/link.ex
@@ -1,17 +1,23 @@
 defmodule ExIPFS.Link do
   @moduledoc false
+
+  @enforce_keys [:/]
   defstruct /: nil
 
   require Logger
 
-  @spec new(any) :: ExIPFS.link()
+  @spec new(map | binary) :: ExIPFS.link()
   def new(cid) when is_map(cid) do
-    case cid do
-      %{"/" => something} ->
-        %{/: something}
 
-      _ ->
-        %{/: cid./}
-    end
+        %__MODULE__{/: cid["/"]}
   end
+
+  def new(cid) when is_binary(cid) do
+    %__MODULE__{/: cid}
+  end
+
+  # Pass on errors.
+
+  @spec new({:error, any}) :: {:error, any}
+  def new({:error, data}), do: {:error, data}
 end
