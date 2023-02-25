87dcf7f0a96774c22ca97293d68ee350b43764c3
Lars Bahner <lars.bahner@gmail.com>
2023-02-25 23:48:52 +0100

    Release small update 0.1.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExIpfs.Mixfile do
   def project do
     [
       app: :ex_ipfs,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       name: "Elixir IPFS",
       start_permanent: Mix.env() == :prod,
